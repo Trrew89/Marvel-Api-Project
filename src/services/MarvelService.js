8384b8161547373287588d75a826501c57250384
Sveatoslav <sveatoslavbodrus@gmail.com>
2023-01-08 17:55:52 +0100

    added error-boundary as an example

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -27,11 +27,13 @@ class MarvelService {
 
     _transormCharacter = (char) => {
         return {
+            id: char.id,
             name: char.name,
-            desription: char.desription ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
             thumbnail: char.thumbnail.path + '.' +char.thumbnail.extension,
             homepage: char.urls[0].url,
-            wiki: char.urls[1].url
+            wiki: char.urls[1].url,
+            comics: char.comics.items
         }
     }
 }
